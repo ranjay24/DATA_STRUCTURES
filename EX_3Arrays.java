34f1d8f837b976d1ec83692859fee2741b517e91
RANJAYSINGH <ran2492002@gmail.com>
2025-09-19 21:54:22 +0530

    Simple Array in java

---
+++ EX_3Arrays.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class EX_3Arrays {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[]  = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] =  in.nextInt();
+            System.out.println(arr[i]);
+        }
+     }
+}
