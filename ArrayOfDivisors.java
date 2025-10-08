b7ae6eb0fc146a53f1f607387f9795752af865fb
RANJAYSINGH <ran2492002@gmail.com>
2025-10-08 23:36:39 +0530

    Added missing imports

--- ArrayOfDivisors.java
+++ ArrayOfDivisors.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+
 class Solution {
   public int[] divisors(int n) {
 
-    ArrayList<Integer> list = new ArrayList<>();
+    ArrayList<Integer> list = new ArrayList<Integer>();
     for (int i = 1; i <= n; i++) {
       if (n % i == 0) {
         list.add(i);
